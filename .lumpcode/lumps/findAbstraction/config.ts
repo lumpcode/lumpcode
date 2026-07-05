d48017465c461220c5b631f8447b23b7577b95a0
Dyodio <danielsaadia@dyod.io>
2026-07-05 19:15:06 +0200

    chore: adding naive version of findAbstraction lump

--- .lumpcode/lumps/findAbstraction/config.ts
+++ .lumpcode/lumps/findAbstraction/config.ts
@@ -2,28 +2,34 @@ import { defineConfig } from '@lumpcode/cli-types';
 
 export default defineConfig({
     getContextListFn: async () => {
-        const name = Date.now().toString();
+        const date = Date.now().toString();
         return [
             {
-                name,
+                name: date,
                 variables: {
-                    NAME: name,
+                    DATE: date,
                 },
             }
         ]
     },
     command: 'cursor',
+    lumpVariables: {
+        model: 'composer-2.5'
+    },
     steps: [
         {
             promptFn() {
                 return `
                     Look at all the files in @packages/apps/cli. Find one abstraction you can do to reduce repetitions in the code. Apply the abstraction.
-                    Write an explanation of the abstraction you applied at @packages/apps/cli/NAME_OF_THE_ABSTRACTION.abstraction.md.
+                    Write an explanation of the abstraction you applied in .lumpcode/lumps/findAbstraction/NAME_OF_THE_ABSTRACTION.abstraction.md.
                 `
             },
         },
     ],
     keepHistory: true,
     verbose: true,
-    disabled: true,
+    disabled: false,
+    numberOfContextsPerBranch: 1,
+    maximumNumberOfConcurrentBranches: 1,
+    discoveryBranch: 'dev',
 })
