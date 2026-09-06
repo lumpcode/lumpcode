1cb923f7d61d96b03c1b1ec6f3b9d34342667990
Dyodio <danielsaadia@dyod.io>
2026-09-06 17:01:25 +0200

    wip

--- packages/apps/website/app/components/AppHeader.vue
+++ packages/apps/website/app/components/AppHeader.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 const route = useRoute()
 const path = computed(() => withoutTrailingSlash(route.path))
+const isDocs = computed(() => path.value.startsWith('/docs'))
+const { open, toggleSidebar } = useDocsSidebar()
 </script>
 
 <template>
@@ -32,6 +34,19 @@ const path = computed(() => withoutTrailingSlash(route.path))
           Docs
         </NuxtLink>
         <a class="nav-optional" :href="githubRepoUrl">GitHub</a>
+        <button
+          v-if="isDocs"
+          class="docs-nav-toggle"
+          type="button"
+          aria-label="Documentation menu"
+          aria-controls="docs-sidebar"
+          :aria-expanded="open"
+          @click="toggleSidebar"
+        >
+          <svg class="docs-nav-burger" viewBox="0 0 18 18" aria-hidden="true">
+            <path d="M3 5h12M3 9h12M3 13h12" />
+          </svg>
+        </button>
       </nav>
     </div>
   </header>
